18f3f62aa13e5d53f9d4de9dc7c86fd21f1eb49e
0plaze0 <banikakash131@gmail.com>
2025-03-25 22:50:00 +0530

    test: unit test for vote update

--- test/election.js
+++ test/election.js
@@ -28,4 +28,38 @@ contract("Election", function(accounts){
         })
     })
 
+    it("allow a voter to cast a vote", function(){
+        return Election.deployed().then(function(instance){
+            electionInstance = instance;
+            candidateId = 1;
+
+           
+            return electionInstance.vote(candidateId, {from: accounts[0]})
+        }).then(function(receipt){
+            return electionInstance.voters(accounts[0]);
+        }).then(function(voter){
+            assert(voter, "Then vote a casted")
+            return electionInstance.candidates(candidateId);
+        }).then(function(candidate){
+            assert.equal(candidate[2], 1, "Vote a incremented");
+        })
+    })
+
+    it("throwing an expection for invalid candidate", function(){
+        return Election.deployed().then(function(instance){
+            electionInstance = instance;
+            candidateId = 99;
+
+            return electionInstance.vote(candidateId, {from: accounts[1]})
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert') >= 0, "Error msg contains revert")
+            return electionInstance.candidates(1);
+        }).then(function(candidate){
+            assert.equal(candidate[2], 1 , "Vote of Candidate 1 was not incremented due to invalid candidate");
+            return electionInstance.candidates(2);
+        }).then(function(candidate){
+            assert.equal(candidate[2], 0 , "Vote of Candidate 2 was not incremented due to error");
+        })
+    })
+
 })
